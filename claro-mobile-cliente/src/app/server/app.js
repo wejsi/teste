1725ad81c6ca069fe280d9aa45672901aba5342c
Weslen <weslen.developed.si@gmail.com>
2018-06-22 20:44:50 -0300

    Ajustando path para nodemon

--- claro-mobile-cliente/src/app/server/app.js
+++ claro-mobile-cliente/src/app/server/app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(__dirname + '/../../dist'));
+
 app.use('/', express.static(__dirname + '/../public'));
 
 app.use(bodyParser.json());
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname,'/../../dist/index.html'));
+  res.sendFile(path.join(__dirname,'/../public/index.html'));
 });
 
 app.listen(app.get('port'), function() {
