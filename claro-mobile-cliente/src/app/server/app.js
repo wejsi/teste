96a0898a4ede1330286d37221bd32e3a5a78ae07
Weslen <weslen.developed.si@gmail.com>
2018-06-22 21:19:46 -0300

    Ajustes path sendFile...

--- claro-mobile-cliente/src/app/server/app.js
+++ claro-mobile-cliente/src/app/server/app.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 
-app.use('/', express.static(__dirname + '/../public'));
+app.use('/', express.static(__dirname + '/../../public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname,'/../public/index.html'));
+  res.sendFile(path.join(__dirname,'/../../public/index.html'));
 });
 
 app.listen(app.get('port'), function() {
