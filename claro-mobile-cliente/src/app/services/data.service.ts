17a8db6a5920e03b4bd69f607b5e768dbd3b8da1
Weslen <weslen.developed.si@gmail.com>
2018-06-22 23:37:41 -0300

    Ajustando auth basic...

--- claro-mobile-cliente/src/app/services/data.service.ts
+++ claro-mobile-cliente/src/app/services/data.service.ts
@@ -11,13 +11,20 @@ export class DataService {
   private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
   private options = new RequestOptions({ headers: this.headers });
   private baseUrl: string;
+  private username: string;
+  private password: string;
 
   constructor(private http: Http) {
     this.baseUrl = 'http://localhot:8080';
+    
+    this.username = 'admin'; 
+    this.password = 'admin';        this.options.headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
+    
+    
   }
 
   getCelulares() {
-    return this.http.get(`${this.baseUrl}/claro/mobile`).map(res => res.json());
+    return this.http.get(`${this.baseUrl}/claro/mobile`,this.options).map(res => res.json());
   }
 
   inserir(celular) {
